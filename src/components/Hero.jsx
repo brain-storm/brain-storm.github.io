b05500646fe57cadf8d30a96ad22f748993be3be
grvsharma1810 <gs8280601@gmail.com>
2021-09-05 14:32:35 +0530

    style: hero border radius

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
 	return (
 		<section className="relative bg-hero-image bg-center bg-no-repeat bg-cover h-screen">
 			{/* // <section className="flex h-screen justify-center items-center"> */}
-			<div className="absolute top-32 left-6" style={{lineHeight:"1.25", borderRadius:"50%",backgroundColor:"white", boxShadow:"0 0 50px 50px white"}}>
+			<div className="absolute top-32 left-6" style={{lineHeight:"1.25",backgroundColor:"white", boxShadow:"0 0 50px 50px white"}}>
 				<h1 style={{fontSize:"calc(50px + 2vw)"}}>
 					<span>Hi, We are</span> <br />
 					<span className="font-bold text-teal-900">Brainstorm</span>
