e64f1899c7b9dcc59be0da383016100d564db2c2
grvsharma1810 <gs8280601@gmail.com>
2021-09-05 14:27:16 +0530

    fix: hero font

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ function Hero() {
 	return (
 		<section className="relative bg-hero-image bg-center bg-no-repeat bg-cover h-screen">
 			{/* // <section className="flex h-screen justify-center items-center"> */}
-			<div className="absolute top-28 left-6" style={{lineHeight:"1.25", borderRadius:"50%",backgroundColor:"white", boxShadow:"0 0 50px 50px white"}}>
-				<h1 style={{fontSize:"calc(60px + 1.75vw)"}}>
+			<div className="absolute top-32 left-6" style={{lineHeight:"1.25", borderRadius:"50%",backgroundColor:"white", boxShadow:"0 0 50px 50px white"}}>
+				<h1 style={{fontSize:"calc(50px + 2vw)"}}>
 					<span>Hi, We are</span> <br />
 					<span className="font-bold text-teal-900">Brainstorm</span>
 				</h1>
