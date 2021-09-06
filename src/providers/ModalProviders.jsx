cb30f2261576f6a02c192f4fb0ba61d3ca616ee8
grvsharma1810 <gs8280601@gmail.com>
2021-09-06 09:07:45 +0530

    feat: features in project modal

--- src/providers/ModalProviders.jsx
+++ src/providers/ModalProviders.jsx
@@ -32,6 +32,11 @@ export const ModalProvider = ({ children }) => {
 		});
 	}
 
+	function handleClose(event) {
+		setOpen(false);
+		setCurrentImageInd(0);
+	}
+
 	return (
 		<ModalContext.Provider value={{ open, setOpen, setProject }}>
 			{children}
@@ -40,7 +45,7 @@ export const ModalProvider = ({ children }) => {
 					as="div"
 					className="fixed z-10 inset-0 overflow-y-auto"
 					initialFocus={cancelButtonRef}
-					onClose={setOpen}
+					onClose={handleClose}
 				>
 					<div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 						<Transition.Child
@@ -120,10 +125,28 @@ export const ModalProvider = ({ children }) => {
 											</div>
 
 											<div className="mt-4">
-												<p className="text-md text-gray-500">
+												<p className="text-md text-gray-700">
 													{project?.description}
 												</p>
 											</div>
+											{project?.features && (
+												<>
+													<div className="mt-4 uppercase text-gray-700 font-bold text-left">Features</div>
+													<ul className="mt-2 text-left list-disc list-inside text-sm text-gray-700">
+														{project?.features.map(
+															(feature) => {
+																return (
+																	<li>
+																		{
+																			feature
+																		}
+																	</li>
+																);
+															}
+														)}
+													</ul>
+												</>
+											)}
 										</div>
 									</div>
 								</div>
@@ -161,7 +184,7 @@ export const ModalProvider = ({ children }) => {
 									<button
 										type="button"
 										className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-										onClick={() => setOpen(false)}
+										onClick={handleClose}
 										ref={cancelButtonRef}
 									>
 										Close
